ceb4f98f0f595351d94fccf71781048f614a5425
LAPTOP-QPO3NJ45\mic <michaelrizkalla@hotmail.com>
2021-12-27 22:27:23 +1100

    fixed rmi by creating registry

--- RMI/MyClient.java
+++ RMI/MyClient.java
@@ -2,19 +2,27 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class MyClient {
     public static void main(String[] args) {
 
     try
     {
-        Adder stub = (Adder) Naming.lookup("rmi://localhost:5000/shubha");
-        System.out.println("Sum ="+stub.add(5,6));
+//        Adder stub = (Adder) Naming.lookup("rmi://localhost:5000/shubha");
+        Registry registry = LocateRegistry.getRegistry(5000);
+        Adder adderStub = (Adder)registry.lookup("Adder");
+        System.out.println("Sum ="+adderStub.add(5,6));
     } catch (RemoteException e) {
+        System.out.println("ERROR");
         System.out.println(e.getMessage());
+        e.printStackTrace();
     } catch (NotBoundException e) {
+        System.out.println("ERROR");
         System.out.println( e.getMessage());
-    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    } /*catch (MalformedURLException e) {
         System.out.println( e.getMessage());
-    }
+    }*/
     }}
--- RMI/MyServer.java
+++ RMI/MyServer.java
@@ -1,17 +1,24 @@
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class MyServer {
     public static void main(String[] args) {
         try
         {
-            Adder stub = new AdderRemote();
-            Naming.rebind("rmi://localhost:5000/shubha",stub);
+//            System.setProperty("java.rmi.server.hostname","localhost");
+                Registry registry = LocateRegistry.createRegistry(5000);
+            registry.rebind("Adder", new AdderRemote());
+//            Naming.rebind("rmi://127.0.0.1:5001/shubha",stub);
         } catch (RemoteException e) {
+            System.out.println("ERROR");
             System.out.println(e.getMessage());
-        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } /*catch (MalformedURLException e) {
             System.out.println(e.getMessage());
-        }
+        }*/
     }
 }
